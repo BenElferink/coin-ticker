ca1b900ff4256f4618ac47de8fe159902e7a29ee
Ben Elferink <ben.elferink@icloud.com>
2023-06-05 13:36:13 +0300

    fix green/red component

--- components/ChangeGreenRed.tsx
+++ components/ChangeGreenRed.tsx
@@ -1,45 +1,30 @@
 import { GREEN, RED } from '@/constants'
 import NumberValue from './NumberValue'
-
-const invertColor = '#f9f9f9'
-
-const CaretUp = ({ size = 16, fill = 'currentColor', style = {} }) => {
-  return (
-    <svg viewBox='0 0 16 16' width={size} height={size} fill={fill} style={style}>
-      <path d='m7.247 4.86-4.796 5.481c-.566.647-.106 1.659.753 1.659h9.592a1 1 0 0 0 .753-1.659l-4.796-5.48a1 1 0 0 0-1.506 0z' />
-    </svg>
-  )
-}
-
-const CaretDown = ({ size = 16, fill = 'currentColor', style = {} }) => {
-  return (
-    <svg viewBox='0 0 16 16' width={size} height={size} fill={fill} style={style}>
-      <path d='M7.247 11.14 2.451 5.658C1.885 5.013 2.345 4 3.204 4h9.592a1 1 0 0 1 .753 1.659l-4.796 5.48a1 1 0 0 1-1.506 0z' />
-    </svg>
-  )
-}
+import { ChevronDownIcon, ChevronUpIcon } from '@heroicons/react/24/solid'
 
 const ChangeGreenRed = ({ value = 0, prefix = '', suffix = '', invert = false, withCaret = false, scale = 1 }) => {
   const changeColor = value > 0 ? GREEN : value < 0 ? RED : 'unset'
+  const invertColor = '#f9f9f9'
 
-  const style = {
-    margin: invert ? `${2 * scale}px ${4 * scale}px` : 'unset',
-    padding: invert ? `${4 * scale}px ${7 * scale}px` : 'unset',
-    backgroundColor: invert ? changeColor : 'unset',
-    color: invert ? invertColor : changeColor,
-    fontSize: `${16 * scale}px`,
-    fontWeight: '500',
-    borderRadius: '7px',
-    display: 'flex',
-    alignItems: 'center',
+  const styles = {
+    root: {
+      backgroundColor: invert ? changeColor : 'unset',
+      color: invert ? invertColor : changeColor,
+      fontSize: `${16 * scale}px`,
+    },
+    icon: {
+      width: 14 * scale,
+      height: 14 * scale,
+      marginRight: `${3 * scale}px`,
+    },
   }
 
   return (
-    <span style={style}>
+    <span className={(invert ? 'py-1 px-2' : 'p-0') + ' flex items-center rounded-lg'} style={styles.root}>
       {withCaret && value > 0 ? (
-        <CaretUp size={10 * scale} fill={invertColor} style={{ marginRight: `${2 * scale}px` }} />
+        <ChevronUpIcon style={styles.icon} />
       ) : withCaret && value < 0 ? (
-        <CaretDown size={10 * scale} fill={invertColor} style={{ marginRight: `${2 * scale}px` }} />
+        <ChevronDownIcon style={styles.icon} />
       ) : null}
 
       <NumberValue prefix={prefix} suffix={suffix} value={Number(String(value).replace('-', ''))} />
